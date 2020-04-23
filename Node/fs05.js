8bea322b4fe1838ce75921323648aca82a61cd36
quadrant26 <quadrant26@163.com>
2020-04-23 14:18:38 +0800

    fs.readFile 读取文件

---
+++ Node/fs05.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+// 追加文件
+/*
+ * filename   string        文件名称
+ * callback      回调函数， 参数为 (err, data)
+*/
+fs.readFile('./css/base.css', (err, data) => {
+    if( err ){
+        console.log(err)
+        return;
+    }
+
+    console.log(data) // Buffer
+    console.log(data.toString()) // 将 Buffer 转化为 string
+})
