903feb11f751ee556451cc6d2af891bdb6d1a822
quadrant26 <quadrant26@163.com>
2020-04-23 14:29:13 +0800

    fs.rename 重命名

--- Node/fs06.js
+++ Node/fs06.js
@@ -1,15 +1,31 @@
 const fs = require('fs');
 
-// 读取目录
+// rename
+// todo 1. 表示重命名
+// todo 2. 移动
 /*
  * filename   string        文件名称
+ * newFilename string 修改后的文件名称
  * callback      回调函数， 参数为 (err, data)
 */
-fs.readdir('./css', (err, data) => {
+
+// todo 重命名
+fs.rename("./css/base.css", './css/index.css', function (err){
     if( err ){
         console.log(err)
         return;
     }
 
-    console.log(data)
+    console.log("文件重命名成功");
+})
+
+
+// todo 移动
+fs.rename("./css/base.css", './html/index.css', function (err){
+    if( err ){
+        console.log(err)
+        return;
+    }
+
+    console.log("移动文件成功");
 })
