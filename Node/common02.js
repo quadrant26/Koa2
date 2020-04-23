fa37733c9b728604039cd1ec6302e8be40ce07d2
quadrant26 <quadrant26@163.com>
2020-04-23 13:29:26 +0800

    模块的导出

---
+++ Node/common02.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const request = require('./module/request.js');
+
+console.log(request)
+
+http.createServer( (req, res) => {
+
+    res.writeHead(200, {"Content-type" : "text/html, charset='utf-8'"});
+
+    res.write("<head><meta charset='utf-8' /></head>");
+
+    res.write("Hello node");
+
+    var api = tools.formatApi('api/focus');
+    res.write(api);
+
+    res.end();
+}).listen(3000);
