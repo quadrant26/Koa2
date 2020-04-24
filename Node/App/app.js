99533dc7e36648d534af5e61b64f0d3d03ad8dbf
quadrant26 <quadrant26@163.com>
2020-04-24 17:57:42 +0800

    get post 数据接收和封装

--- Node/App/app.js
+++ Node/App/app.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const app = require('./module/routes');
+const ejs = require('ejs');
 
 http.createServer(app).listen(3000);
 
@@ -11,9 +12,18 @@ app.get('/', function (req, res){
 })
 
 app.get('/login', function (req, res){
+    // res.write("<head><meta charset='utf-8' /></head>");
+    // res.end('执行登录操作')
+    ejs.renderFile('./views/form.ejs', {}, (err, data) => {
+        res.writeHead(200, {"Content-type": "text/html; charset='utf-8'"});
+        res.end(data);
+    })
+})
+
+app.post("/doLogin", (req, res) => {
+    console.log("doLogin")
     res.writeHead(200, {"Content-type": "text/html; charset='utf-8'"});
-    res.write("<head><meta charset='utf-8' /></head>");
-    res.end('执行登录操作')
+    res.end(req.body);
 })
 
 console.log("The server is http://127.0.0.1:3000");
--- Node/App/module/routes.js
+++ Node/App/module/routes.js
@@ -1,23 +1,49 @@
 const url = require('url');
-const path = require('path');
 
-let G = {};
+let server = () => {
+    let G = {};
+    // 分割 get 和 post 请求
+    G._get = {};
+    G._post = {};
 
-let app = function (req, res){
-    
-    let pathname = url.parse(req.url).pathname;
+    let app = function (req, res){
 
-    if(G[pathname]){
-        G[pathname](req, res);
-    }else{
-        res.writeHead(404, {"Content-type": "text/html; charset='utf-8'"});
-        res.end('页面不存在')
+        let pathname = url.parse(req.url).pathname;
+        // 获取请求类型
+        let method = req.method.toLowerCase();
+        if(G['_'+method][pathname]){
+            if ( method == "get"){
+                G._get[pathname](req, res);
+            }else{
+                // 获取post数据
+                let postData = '';
+                req.on("data", function (chunkdata){
+                    postData += chunkdata;
+                })
+                req.on("end", () => {
+                    req.body = postData;
+                    G._post[pathname](req, res);
+                })
+            }
+            
+        }else{
+            res.writeHead(404, {"Content-type": "text/html; charset='utf-8'"});
+            res.write('<head><meta charset="utf-8" /></head>');
+            res.end('页面不存在')
+        }
     }
+
+    app.get = function (str, cb){
+        // 注册方法
+        G._get[str] = cb;
+    }
+
+    app.post = function (str, cb){
+        G._post[str] = cb
+    }
+
+    return app;
 }
 
-app.get = function (str, cb){
-    // 注册方法
-    G[str] = cb;
-}
 
-module.exports=app;
+module.exports=server();
