c9c9999bc9a3d01e63c8f35e276fd9456d8272c4
quadrant26 <quadrant26@163.com>
2020-04-27 16:32:06 +0800

    原生获取 post 传输的数据

---
+++ Koa/module/common.js
@@ -0,0 +1,19 @@
+exports.getPostData = function (ctx) {
+    // 获取数据 异步
+    return new Promise ( (resolve, reject) => {
+
+        try{
+            let str = '';
+            ctx.req.on("data", function (data){
+                str += data;
+            })
+
+            ctx.req.on("end", function (){
+                resolve(str);
+            })
+        } catch (err) {
+            reject(err)
+        }
+        
+    })
+}
