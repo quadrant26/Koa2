6a503b56d47f22d8c4bbcc48f8300fd478843b23
quadrant26 <quadrant26@163.com>
2020-04-27 15:08:02 +0800

    动态路由

---
+++ Koa/app-router-dynamic.js
@@ -0,0 +1,63 @@
+const Koa = require('koa');
+const Router = require('koa-router')
+
+// 实例化
+const app = new Koa();
+// 实例化路由
+const router = new Router();
+
+
+// 配置路由
+router.get("/", async (ctx) => {
+    ctx.body = "首页"
+})
+
+router.get("/news", async (ctx) => {
+    ctx.body = "新闻页"
+})
+
+// 动态路由
+router.get("/newscontent/:aid/:uid", async (ctx) => {
+    
+    console.log(ctx.params);
+    
+
+    ctx.body = "新闻详情"
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app
+    .use(router.routes())    // 启动路由
+    .use(router.allowedMethods());  // 建议配置
+
+/*
+ * router.allowedMethods()
+ * 当所有路由中间件最后调用， 此时根据 ctx.statuus 设置 response 响应头 
+ */
+
+
+
+app.listen(3000)
