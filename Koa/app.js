71141a290a92307d294181635c1e4502feffed4d
quadrant26 <quadrant26@163.com>
2020-04-28 16:38:42 +0800

    koa 各类中间件使用

--- Koa/app.js
+++ Koa/app.js
@@ -1,20 +1,48 @@
 const Koa = require('koa');
+const Router = require('koa-router');
+const session = require('koa-session')
 
 const app = new Koa();
+const router = new Router();
 
+// 配置session的中间件
+app.keys = ['some secret hurr'];    // cookie 的签名
+const CONFIG = {
+    key: 'koa:sess', /** 默认 */
+    maxAge: 86400000, /** cookie 的过期时间 需要设置 */
+    autoCommit: true, /** (boolean) automatically commit headers (default true) */
+    overwrite: true, /** (boolean) can overwrite or not (default true) */
+    httpOnly: true, /** true 只有服务端才可以获取 */
+    signed: true, /** 默认签名 (default true) */
+    rolling: false, /** (boolean) 每次访问都会强制设置cookie (default is false) */
+    renew: false, /** (boolean)  快过期重新设置. (default is false) 需要修改*/
+    sameSite: null, /** (string) session cookie sameSite options (default null, don't set it) */
+};
+app.use(session(CONFIG, app));
 
-// 配置路由
-
-// 中间件
-
-// express 写法
-// app.use(function (res, res){
-//     res.send("返回数据")
-// })
-
-
-app.use( async (ctx) => {
-    ctx.body = ("您好， Koa");
+router.get('/', async (ctx) => {
+    
+    // 获取 session
+    let userinfo = ctx.session.userinfo
+    ctx.body = "这是首页" + userinfo;
 })
+
+router.get("/login", async (ctx) => {
+    // 设置 session
+    ctx.session.userinfo = "zhangsan";
+    ctx.body = "登录成功";
+})
+
+router.get("/news", async (ctx) => {
+    // 设置 session
+    // 获取 session
+    let userinfo = ctx.session.userinfo
+    ctx.body = "这是新闻" + userinfo;
+})
+
+app
+    .use(router.routes())
+    .use(router.allowedMethods());
+
 
 app.listen(3000)
